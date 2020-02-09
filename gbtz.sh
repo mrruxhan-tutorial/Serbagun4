fa5d848b31500ff498bee0389fb333e6d7cb9bcb
MRRUXHAN <58897954+mrruxhan-tutorial@users.noreply.github.com>
2020-02-09 13:03:59 +0700

    Add files via upload

---
+++ gbtz.sh
@@ -0,0 +1,129 @@
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+mer='\033[41;97m' #Tepi
+st='\033[0m' #Stop
+R='\x1b[1;31m'
+G='\x1b[1;32m'
+B='\x1b[1;34m'
+Y='\x1b[1;33m'
+C='\x1b[1;36m'
+D='\x1b[0m'
+
+
+#Mod by        : M Ruxhan
+#Tool Unfaedah : Hehe;"v"
+
+clear
+            
+            echo $pur"  ╦ ╦╔╗╔╔═╗╔═╗╔═╗╔╦╗╔═╗╦ ╦   "      
+            echo $pur"  ║ ║║║║╠╣ ╠═╣║╣  ║║╠═╣╠═╣"
+            echo $pur"  ╚═╝╝╚╝╚  ╩ ╩╚═╝═╩╝╩ ╩╩ ╩"
+            echo $mer"  ×××Tool Unfaedah | M Ruxhan $st"
+
+lagi=1
+while [ $lagi -lt 6 ];
+do
+echo""
+echo $cy"1. Sadap Whatsapp ";
+echo $pur"====================]" 
+echo $cy"2. Hack Facebook";
+echo $pur"====================]" 
+echo $cy"3. Hack Instagram";
+echo $pur"====================]" 
+echo $cy"4. Hack Twitter";
+echo $pur"====================]" 
+echo $cy"5. Sadap Instagram";
+echo $pur"====================]" 
+echo $cy"6. Sadap Facebook";
+echo $pur"====================]"        
+echo $i"7. EXIT $st";
+echo ""
+echo    $mer"╭─Pilih Nomor Berapa Bep!!? $st " 
+read -p "╰─•>  " pil; 
+
+case $pil in
+1) clear
+echo $i "
+██████╗ █████╗ ██████╗  █████╗ ██████╗ 
+██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗
+███████╗███████║██║  ██║███████║██████╔╝
+╚════██║██╔══██║██║  ██║██╔══██║██╔═══╝ 
+███████║██║  ██║██████╔╝██║  ██║██║     
+╚══════╝╚═╝  ╚═╝╚═════╝ ╚═╝  ╚═╝╚═╝     
+                                        
+
+"
+echo ""
+echo -n $cy"Isi Nomor Target:"
+read tr
+echo "============================" | lolcat
+echo -n $cy"isi nomor lu : "
+read lu
+clear
+echo "Sabar Bep lagi nyadap ini $tr" | lolcat
+echo ""
+echo $mer " Tunggu Sekitar 1menit ya Bep $st"
+echo ""
+sleep 25
+echo $cy " Sedang Mengambil Chat Terbaru Om"
+sleep 9
+echo $bi " Chat Terbaru Dengan '+6285711982738' "
+echo $cy  " <Kemarin> "
+echo ""
+echo $pu "+6285711982738: Selamat Pagi Sayang$cy[08:02]"
+echo $i "+6288290399954: Pagi Juga Sayang $cy[08:04]"
+echo $pu "+6285711982738: Lagi Apa? $cy[08:07]"
+echo $i "+6288290399954 : Tiduran Aja $cy[08:09]"
+echo $pu "+6285711982738 : Udah Mandi? $cy[08:11]"
+echo $i "+6288290399954 : Belum, Kamu Udah Mandi Sama Makan? $cy[08:15]"
+echo $pu "+6285711982738 : Belum Sama Sekali $cy[08:18]"
+echo $i "+6288290399954 : Aku Off Dulu Ya $cy[08:22]"
+echo $pu "+6285711982738 : Oke Oke $cy[08:39]"
+echo $pu "+6288290399954 : Selamat Sore $cy[15:11]"
+echo $i "+6285711982738 : Sore Juga Sayang $cy[15:17]"
+echo $pu "+6288290399954: Jalan² Yuk Bosen Nih $cy[15:19]"
+echo $i "+6285711982738 :Yuk, Aku Tunggu Di Taman $cy[15:29]"
+echo $pu "+6288290399954: Oke, Say... Otw😘😘 $cy[15:32]"
+exit
+;;
+
+2) clear
+sh 2.sh
+exit
+;;
+
+3) clear
+sh 3.sh
+exit
+;;
+
+4) clear
+sh 4.sh
+exit
+;;
+
+5) clear
+sh 5.sh
+exit
+;;
+
+6) clear
+sh 6.sh
+exit
+;;
+
+7) echo $cy " Bye Bye Bep Tools Gabut Yekan"
+exit
+;;
+*) echo " Tools Gabut Kan🤣" | lolcat
+esac
+done
+done
+exit
+
